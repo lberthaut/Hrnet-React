5b191c8d32c301b4993617a32fd2e540ea7b8432
Louis Berthaut <louis.berthaut@gmx.com>
2022-01-22 18:12:46 +0100

    add clear states function

--- hrnet-react/src/components/header.js
+++ hrnet-react/src/components/header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 /*Header component, displaying new array form, and employee array*/
 export default function Header() {
+
+
+    function refreshPage() {
+        const navigate = useNavigate
+        navigate("/")
+    }
 
     return (
         <div className="header">
             <h1 className="title-header">HRnet</h1>
             <div className="header-nav">
-                <Link to="/" className="createuser">Create Employee</Link>
+                <Link to="/" className="createuser" onClick={refreshPage}>Create Employee</Link>
                 <Link to="/employeeslist" className="employeeslist">Employees List</Link>
             </div>
         </div>
